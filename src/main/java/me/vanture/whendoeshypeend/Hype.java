a1b7bb3e9c4ab86e371fd1c0004832430bd5eb10
Tim <vanturetim@gmail.com>
2016-06-29 21:51:20 +0200

    Initial commit

---
+++ src/main/java/me/vanture/whendoeshypeend/Hype.java
@@ -0,0 +1,27 @@
+package me.vanture.whendoeshypeend;
+
+import java.util.Date;
+
+/**
+ * @author vanture
+ * @since 21/06/2016
+ */
+public class Hype {
+    public static void main(String[] args) {
+
+        // When does the Hype really end? Well it will finally be calculated!
+        long H = 'H';
+        long y = 'y';
+        long p = 'p';
+        long e = 'e';
+
+        long hype = 1337; //You're a l33t to?
+        hype = (long) Math.pow(hype, H);
+        hype = (long) Math.pow(hype, y);
+        hype = (long) Math.pow(hype, p);
+        hype = (long) Math.pow(hype, e);
+
+        System.out.println("When does HYPE end?!?!?");
+        System.out.println(new Date(hype));
+    }
+}
